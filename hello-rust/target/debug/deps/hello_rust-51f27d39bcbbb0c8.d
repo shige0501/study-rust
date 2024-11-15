8a8391c8ba5358cd7d27c0535b49259874b133a5
Koji Shigemura <shige0501@gmail.com>
2024-11-15 22:41:05 +0900

    fixes launch.json.

---
+++ hello-rust/target/debug/deps/hello_rust-51f27d39bcbbb0c8.d
@@ -0,0 +1,5 @@
+/Users/shige0501/work/study-rust/hello-rust/target/debug/deps/hello_rust-51f27d39bcbbb0c8: src/main.rs
+
+/Users/shige0501/work/study-rust/hello-rust/target/debug/deps/hello_rust-51f27d39bcbbb0c8.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/shige0501/work/study-rust/hello-rust/target/debug/hello-rust: /Users/shige0501/work/study-rust/hello-rust/src/main.rs
